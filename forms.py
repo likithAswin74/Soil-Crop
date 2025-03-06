b31854e767ec8dea25feca517808622665e19666
Likith Aswin <likithaswin14@gmail.com>
2025-03-06 19:59:37 +0530

    commited successfully

--- forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, TextAreaField, EmailField, FileField
-from wtforms.validators import DataRequired, Email, Length, Regexp
-from flask_wtf.file import FileAllowed
-
-
--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def admin_only(function):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "lsdfeownofflrwerowjsdskfjsowiohsorjoitew"
+app.config['SECRET_KEY'] = os.environ.get("Soil_Crop_Flask_Secretkey")
 Bootstrap5(app)
 
 
@@ -90,7 +90,7 @@ class Base(DeclarativeBase):
 
 
 # configuring and initilizing the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tables.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("Soil_Crop_Database_SecretKey")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -122,6 +122,67 @@ class Specialist(UserMixin, db.Model):
     specialization: Mapped[str] = mapped_column(String(100))
 
 
+class Soil(db.Model):
+    __tablename__ = "soil"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=False)
+    texture: Mapped[str] = mapped_column(String(50), nullable=False)
+    nutrients: Mapped[str] = mapped_column(String(255), nullable=False)
+    best_suited_crops: Mapped[str] = mapped_column(String(255), nullable=False)
+    image: Mapped[str] = mapped_column(String(255), nullable=True)  # Image path
+
+
+class Crop(db.Model):
+    __tablename__ = "crop"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=False)
+    soil_type: Mapped[str] = mapped_column(String(255), nullable=False)
+    growing_season: Mapped[str] = mapped_column(String(100), nullable=False)
+    water_requirement: Mapped[str] = mapped_column(String(50), nullable=False)
+    image: Mapped[str] = mapped_column(String(255), nullable=True)  # Image path
+
+
+# if the specialist fill the form of the crop details, then it is stored here. and visible to admin
+class DistributorCropDetails(db.Model):
+    __tablename__ = "distributor_crop_details"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    distributor_name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    crop_name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    location: Mapped[str] = mapped_column(String(50), nullable=False)
+    soil_type: Mapped[str] = mapped_column(String(255), nullable=False)
+    growing_season: Mapped[str] = mapped_column(String(100), nullable=False)
+    address: Mapped[str] = mapped_column(String(100), nullable=False)
+    image: Mapped[str] = mapped_column(String(255), nullable=True)  # Image path
+    contact_number: Mapped[str] = mapped_column(Integer, nullable=True)
+
+
+# if the specialist fill the form of the soil details, then it is stored here. and visible to admin
+class DistributorSoilDetails(db.Model):
+    __tablename__ = "distributor_soil_details"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    distributor_name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    soil_name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    location: Mapped[str] = mapped_column(String(50), nullable=False)
+    address: Mapped[str] = mapped_column(String(250), nullable=False)
+    nutrients: Mapped[str] = mapped_column(String(255), nullable=False)
+    best_suited_crops: Mapped[str] = mapped_column(String(255), nullable=False)
+    image: Mapped[str] = mapped_column(String(255), nullable=True)  # Image path
+    contact_number: Mapped[str] = mapped_column(Integer, nullable=True)
+
+
+class Details(db.Model):
+    __tablename__ = "details"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    distributor_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    location: Mapped[str] = mapped_column(String(50), nullable=False)
+    address: Mapped[str] = mapped_column(String(250), nullable=False)
+    number: Mapped[int] = mapped_column(Integer, nullable=False)
+
+
+
 # creating the tables in the database
 with app.app_context():
     db.create_all()
@@ -144,7 +205,11 @@ class AdminModelView(ModelView):
 
 # This creates a new administrative interface for the User model. The first parameter, User, is the model you want to manage. The second parameter, db.session, is the SQLAlchemy session used for database transactions.
 # ModelView creates an interface in the /admin panel to manage (view, add, edit, delete) your database models.
-admin.add_view(AdminModelView(Users, db.session))  # this calls the functions inside the class AdminModelView
+admin.add_view(AdminModelView(Soil, db.session))  # this calls the functions inside the class AdminModelView
+admin.add_view(AdminModelView(Crop, db.session))
+admin.add_view(AdminModelView(DistributorSoilDetails, db.session))
+admin.add_view(AdminModelView(DistributorCropDetails, db.session))
+admin.add_view(AdminModelView(Details, db.session))
 
 
 @app.route("/")
@@ -152,22 +217,39 @@ def landingpage():
     # if the user is not authenticated then show the login and register page
     if not current_user.is_authenticated:
         return render_template("landingpage.html")
-    # if the user is authenticated then the login and register page should not be shown
+
+    # if the user is authenticated then the login and register page should not be shown.
+    # and home function route gets role as a path parameter.(optional) we can also make it as query parameter.
+    # so we have to pass it. if we didnt pass then the role will be None. role is used in base.html
+    # index.html page will be rendered by the home function. if the user is authenticated then we have current_user. so check current_user with isinstance
     else:
-        return redirect(url_for("home"))
+        role = None
+        # role is used to show contact_admin for specialist in the index.html. and show the admin name if the role is admin
+        # so it is not used for any thing except this one.
+        if isinstance(current_user, Specialist):
+            role = "specialist"
+        if isinstance(current_user, AdminUser):
+            role = "admin"
+
+        # why i didnt give for user, because though he is a logged in user he might be an user.
+        return redirect(url_for("home", role=role))
 
 
 @app.route("/home")
 def home():
 
-    # only allows the user when he is logged in.
+    # only allows the user when he is logged in.don't allow if he is not authenticated
     if not current_user.is_authenticated:
         flash("First Log in and try again!")
         return redirect(url_for("landingpage"))
 
     role = request.args.get("role")
+    print(role)
+    # need to send the database details to the index.html for soil and crops details.
+    soils = db.session.execute(db.select(Soil).order_by(Soil.name)).scalars().all()
+    crops = db.session.execute(db.select(Crop).order_by(Crop.name)).scalars().all()
 
-    return render_template("index.html", role=role, logged_in=current_user.is_authenticated)
+    return render_template("index.html", role=role, soils=soils, crops=crops, logged_in=current_user.is_authenticated)
     # things happening in base.html
     # if the user == "admin" then we need to display the admin name
     # if the  user == "specialist" then we need to display the admin name and also contact admin
@@ -193,11 +275,9 @@ def register():
         # Find user and specialist by email entered.
         if form_name and form_name == "user":
             email_in_database = db.session.execute(db.select(Users).where(Users.email == email)).scalar()
-            user_id = f"user_{email_in_database.id}"  # Composite ID for user
 
         else:  # check for specialist
             email_in_database = db.session.execute(db.select(Specialist).where(Specialist.email == email)).scalar()
-            user_id = f"specialist_{email_in_database.id}"  # Composite ID for specialist
             role = "specialist"
 
         # check if email exists
@@ -226,7 +306,7 @@ def register():
             # storing the composite user_id in the session to ensure which user is login in . user or admin or specialist
             # eg: user_1 or specilaist_1 or admin_1.the user_id looks like this.
             # it can be taken by the loaduser callback and takes the id form the table of the logged in user.
-            session["user_id"] = user_id
+            session["user_id"] = f"user_{new_user.id}"
 
         else:
             new_user = Specialist(
@@ -244,7 +324,7 @@ def register():
             # storing the composite user_id in the session to ensure which user is login in . user or admin or specialist
             # eg: user_1 or specilaist_1 or admin_1.the user_id looks like this.
             # it can be taken by the loaduser callback and takes the id form the table of the logged in user.
-            session["user_id"] = user_id
+            session["user_id"] = f"specialist_{new_user.id}"
 
         return redirect(url_for("home", role=role))
 
@@ -313,6 +393,108 @@ def login():
 def logout():
     logout_user()  # logout the user by removing the user id from the session
     return redirect(url_for('landingpage'))
+
+
+# Contact Admin Route
+# it is route which is triggered when the user is a specialist and he clicks the contact_admin button shown to him
+# here the content from the form is stored in the messages database which is only shown to the admin when he goes to admin panel
+@app.route('/contact-admin', methods=['GET', 'POST'])
+def contact_admin():
+
+    # if the user is not authenticated then show the login and register page
+    if not current_user.is_authenticated:
+        return render_template("landingpage.html")
+
+    # if the user is authenticated then the login and register page should not be shown.
+    # role is used in base.html
+    # contact_admin page rendered. if the user is authenticated then we have current_user. so check current_user with isinstance
+    else:
+        role = None
+        # role is used to show contact_admin for specialist in the index.html. and show the admin name if the role is admin
+        # so it is not used for any thing except this one.
+        if isinstance(current_user, Specialist):
+            role = "specialist"
+        if isinstance(current_user, AdminUser):
+            role = "admin"
+
+        if request.method == 'POST':
+            if request.args.get("form_name") == "soil":  # If Soil Form is submitted
+                distributor_name = request.form["distributor_name"]
+                soil_name = request.form['soil_name']
+                location = request.form['location']
+                address = request.form['address']
+                nutrients = request.form['nutrients']
+                best_suited_crops = request.form['best_suited_crops']
+                image = request.form['image']
+                number = request.form['number']
+
+                distributor_soil_details = DistributorSoilDetails(distributor_name=distributor_name,
+                                                                  soil_name=soil_name,
+                                                                  nutrients=nutrients,
+                                                                  best_suited_crops=best_suited_crops,
+                                                                  image=image,
+                                                                  location=location,
+                                                                  address=address,
+                                                                  contact_number=number
+                                                                  )
+
+                db.session.add(distributor_soil_details)
+                db.session.commit()
+                flash('Soil details added successfully!', 'success')
+                # return redirect(url_for("contact_admin"))
+
+            elif request.args.get("form_name") == "crop":   # If Crop Form is submitted
+                distributor_name = request.form["distributor_name"]
+                crop_name = request.form['crop_name']
+                location = request.form['location']
+                soil_type = request.form['soil_type']
+                address = request.form['address']
+                growing_season = request.form['growing_season']
+                image = request.form['image']
+                number = request.form['number']
+
+
+                distributor_crop_details = DistributorCropDetails(distributor_name=distributor_name,
+                                                                  crop_name=crop_name,
+                                                                  soil_type=soil_type,
+                                                                  growing_season=growing_season,
+                                                                  image=image,
+                                                                  location=location,
+                                                                  address=address,
+                                                                  contact_number=number
+                                                                  )
+
+                db.session.add(distributor_crop_details)
+                db.session.commit()
+                flash('Crop details added successfully!', 'success')
+
+        return render_template('contact_admin.html', role=role, logged_in=current_user.is_authenticated)
+
+
+# this route is used to show the soil and crop details
+@app.route("/details")
+def details():
+    # if the user is not authenticated then show the login and register page
+    if not current_user.is_authenticated:
+        return render_template("landingpage.html")
+
+    # if the user is authenticated then the login and register page should not be shown.
+    # role is used in base.html
+    # contact_admin page rendered. if the user is authenticated then we have current_user. so check current_user with isinstance
+    else:
+        role = None
+        # role is used to show contact_admin for specialist in the index.html. and show the admin name if the role is admin
+        # so it is not used for any thing except this one.
+        if isinstance(current_user, Specialist):
+            role = "specialist"
+        if isinstance(current_user, AdminUser):
+            role = "admin"
+
+        # getting the name of the soil or crop to fetch it from the db
+        name = unquote(request.args.get("name"))
+        details_db = db.session.execute(db.select(Details).where(Details.name == name)).scalars().all()
+
+        return render_template('details.html', role=role, distributors=details_db, logged_in=current_user.is_authenticated)
 
 
 if __name__ == "__main__":
